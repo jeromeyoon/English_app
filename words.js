79ac4e7c1a29e026a9979663f076694574c6ae0a
jeromeyoon <vmax8322@gmail.com>
2026-06-10 06:10:52 +0900

    Move default words to words js

---
+++ words.js
@@ -0,0 +1,12 @@
+// Edit the default word list here.
+// Use commas between English words. Two or more letters is recommended.
+var DEFAULT_WORDS = `a, me, go, am, and, which, now, big, one, school,
+the, we, come, are, but, how, today, small, two, teacher,
+I, he, see, can, or, what, tomorrow, good, three, class,
+you, she, look, do, so, who, yesterday, bad, many, friend,
+is, they, like, want, for, yes, always, hot, much, book,
+it, this, play, need, with, no, often, cold, more, desk,
+to, that, have, know, from, please, sometimes, fast, most, home,
+in, your, make, think, of, student, again, slow, all, family,
+on, his, get, use, at, when, then, happy, some, time,
+my, her, help, take, about, where, soon, sad, every, pencil, dam, van, jet, hen, lip, top, mud`;
